6aaf36aa28af2079f695a1c1902b2f19ee328c8d
grzesjan <grzegorzjanusz88@gmail.com>
2016-09-27 19:07:47 +0200

    add bracket to else

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+function getTriangleArea(a, h) {
+    
+    if ((a > 0) && (h > 0)) {
+        return a * h / 2;
+    } else {
+        console.log('nieprawidłowa wartość parametrów');
+    }
+}
+
+var result = getTriangleArea(10, 6);
+console.log(result);
+
+var result1 = getTriangleArea(-10, 6);
+console.log(result1);
+
+var result2 = getTriangleArea(0, 6);
+console.log(result2);
