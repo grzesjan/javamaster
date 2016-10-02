f6b90cb04344ab7d782dbc5d10dbc48e664de53e
grzesjan <grzegorzjanusz88@gmail.com>
2016-10-02 09:56:59 +0200

    add ternary operator

--- java3/scripts.js
+++ java3/scripts.js
@@ -1,11 +1,9 @@
 const a = 3;
-const b = 25;
+const b = 1;
 const value = (a * a) + (2 * a * b) - (b * b);
 
 if (value < 0) {
     console.log('wynik ujemny');
-} else if (value > 0) {
-    console.log('wynik dodatni');
 } else {
-    console.log('wynik jest rowny 0');
+    value > 0 ? console.log('wynik dodatni') : console.log('wynik równy zero');
 }
