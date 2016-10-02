10999d165a7eb2bbe62baa3eca6ba56fb879320d
grzesjan <grzegorzjanusz88@gmail.com>
2016-10-02 11:09:51 +0200

    add style.css and changed function

--- java8/scripts.js
+++ java8/scripts.js
@@ -1,17 +1,17 @@
-document.getElementById('addElem').style.backgroundColor = "rgba(91, 191, 221, 1)";
-document.getElementById('addElem').style.color = "white";
-document.getElementById('addElem').style.borderRadius = "5%";
-document.getElementById('addElem').style.fontSize = "22px";
-document.getElementById('addElem').style.height = "50px";
-document.getElementById('addElem').style.marginLeft = "20px";
 document.getElementById('container').style.display = "flex";
-document.getElementById('container').style.width = "570px";
 
-var add = document.getElementById('addElem');
-var list = document.getElementById('Lista');
+var addItems = document.getElementById('item');
+var list = document.getElementById('lista');
 var i = 1;
 
-add.addEventListener('click', function() {
-    list.innerHTML += '<li>item ' + i++ + '</li>';
+addItems.classList.add('button');
+
+addItems.addEventListener('click', function() {
+    var liItem = document.createElement('li');
+    var text = 'item ' + i++;
+
+    liItem.innerHTML = text;
+    
+    list.appendChild(liItem); 
 });
 
