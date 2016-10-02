664b0d03c85129b3861f2e529393274bfe4f0098
grzesjan <grzegorzjanusz88@gmail.com>
2016-10-02 10:21:43 +0200

    add loops to function rysujChoinke

--- java7/scripts.js
+++ java7/scripts.js
@@ -1,14 +1,13 @@
-function rysujChoinke(numbersOfRows){
-    return numbersOfRows;
-}
-
-var k = rysujChoinke(9);
-
-  for (var i = 0 ; i < k ; i++) {
+function rysujChoinke(numberOfRows){
+    for (var i = 0 ; i < numberOfRows ; i++) {
     var star = " ";
       for (var n = 0 ; n < i * 2 + 1 ; n++) {
         star += '*';
       }
       console.log(star);
-  }
+    }
+}
 
+rysujChoinke(5);
+
+  
